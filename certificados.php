c86f601a6859d9f553d696d1333d1d6331326f85
cheloaz666 <40243014+cheloaz666@users.noreply.github.com>
2020-08-30 13:27:58 -0400

    Add files via upload

---
+++ certificados.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="icon" type="icon/png" href="https://img2.freepng.es/20180329/poq/kisspng-paper-printing-computer-icons-print-5abd8ef67d1835.5215254315223723425124.jpg">
+	<title>Imprimir Certificados</title>
+</head>
+<body>
+	<nav>
+		<h1>IMPRESIÓN DE CERTIFICADOS</h1>
+	</nav>
+</body>
+</html>
